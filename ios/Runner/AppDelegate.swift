082509c9f5a2e30b355dfd54fd5651c6b2ff6e0f
kei1115 <keifukuda1115@gmail.com>
2022-07-17 04:01:55 +0900

    Add setting to set preferred orientation

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,4 +10,10 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  override func application(
+    _ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?
+  ) -> UIInterfaceOrientationMask { return UIInterfaceOrientationMask(arrayLiteral:
+    [UIInterfaceOrientationMask.allButUpsideDown]);
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_engineer_codecheck/home/home_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(
+  WidgetsFlutterBinding.ensureInitialized();
+  //向き指定(縦)
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp],
+  ).then((_) => runApp(
     const ProviderScope(
       child: MyApp(),
     ),
-  );
+  ));
 }
 
 class MyApp extends StatelessWidget {
